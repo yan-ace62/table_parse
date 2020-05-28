3027cf582da5026b58bddebc835e002b35956d59
wenyan <38411792+yan-ace62@users.noreply.github.com>
2020-05-28 11:46:46 +0800

    Update parser_configure.py

--- parser_configure.py
+++ parser_configure.py
@@ -1,21 +1,28 @@
-#!/usr/bin/env python3
-
-#
+  #
 # bitfield manipulation
 #
 
 # bfd session key(bigdian): 0000F95C
 # bfd session result: 00000011 00000000
+# key + result: 0000F95C0000001100000000
+
+#!/usr/bin/env python3
 
 import json
+import argparse
 
-def get_interval_val(value, low , upper):
-    interval = upper - low
-    dst = value >> low
-    modulo = (1 << (interval + 1)) - 1
+def load_configure():
+    files = open("configure.json")
+    config_data = json.load(files)
+    return config_data
 
-    return dst & modulo
+def make_argparse():
+    parser = argparse.ArgumentParser(description='Parse np table.')
+    parser.add_argument('--key', action='store_true', help='Parse np key.')
+    parser.add_argument('--result', action='store_true', help='Parse np result.')
+    parser.add_argument('--n', action='store', metavar='number', help='the position of parsing.')
 
+    return parser
 
 class bf(object):
     def __init__(self, value=0):
@@ -25,22 +32,88 @@ class bf(object):
 
         return self._d[key]
 
-def main():
-    fpt = open("configure.json")
-    js = json.load(fpt)
-    configure = bf(js)
+    def key(self):
+        return self._d["key"]
 
-    keys_str = input("Input Table Data:")
+    def result(self):
+        return self._d["result"]
 
-    numbers = len(keys_str) // 8
+def get_interval_val(value, low , upper):
+    interval = upper - low
+    dst = value >> low
+    modulo = (1 << (interval + 1)) - 1
 
+    return dst & modulo
+
+def do_parse_key(int_list, key_config):
+    loop_number = min(len(int_list), len(key_config))
+    key_obj = {}
+
+    for ind in range(loop_number):
+        keys = int_list[ind]
+
+        for key, value in key_config[ind].items():
+            value = get_interval_val(keys, value[0], value[1])
+            key_obj[key] = value
+
+    return key_obj
+             
+
+def do_parse_result(int_list, result_configure):
+    loop_number = min(len(int_list), len(result_configure))
+
+    for ind in range(loop_number):
+        keys = int_list[ind]
+
+        for key, value in result_configure[ind].items():
+            print(key)
+            print(":")
+            print(get_interval_val(keys, value[0], value[1]))
+
+def do_parse_table(argument, int_list, configure):
+    if argument['key'] & argument["result"]:
+        return False
+    elif argument['key']:
+        key_obj = do_parse_key(int_list, configure["key"])
+        print(key_obj)
+    elif argument["result"]:
+        do_parse_result(int_list, configure["result"])
+    else:
+        do_parse_key(int_list, configure["key"])
+        if len(int_list) > len(configure["key"]):
+            int_list = int_list[len(configure["key"]):]
+            do_parse_result(int_list, configure["result"])
+
+def do_parse_input_data(str_data):
+    numbers = len(str_data) // 8
+    int_list = []
     for ind in range(numbers):
-        keys = int("0x" + keys_str[ind: ind + 8], base=16)
-        print(keys)
+        int_list.append(int(str_data[ind: ind+8], base=16))
 
-        for key, value in configure[ind].items():
-            print_val = get_interval_val(keys, value[0], value[1])
-            print(key + " :" + str(print_val))
+    return int_list
+
+def main():
+    config_data = load_configure()
+    configure = bf(config_data)
+
+    parser = make_argparse()
+
+    argument = parser.parse_args()
+
+    argument = vars(argument)
+
+    #print(type(vars(argument)))
+    #print(vars(argument))
+
+    #print(configure.key())
+    #print(configure.result())
+    #print(configure["key"])
+
+    table_data = input("Input Table Data:")
+
+    int_list = do_parse_input_data(table_data)
+    print(int_list)
+    do_parse_table(argument, int_list, configure)
 
 
 if __name__ == "__main__":
